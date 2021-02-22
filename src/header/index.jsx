aa8261fca48a4c6f76ee2bfe3d27eacbde7ef118
f19m <leomelik@gmail.com>
2021-02-22 13:45:29 +0500

    init: init project

---
+++ src/header/index.jsx
@@ -0,0 +1,16 @@
+import logo from '../assets/logo.png';
+
+
+function header() {
+  return (
+    <header className="header">
+      <div className="container">
+      <div className="header__inner">
+        
+      </div>
+      </div>
+    </header>
+  );
+}
+
+export default App;
